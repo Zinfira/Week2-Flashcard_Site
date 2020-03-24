9520f41b765af80a767d59e14ec6689e8b715840
William Pratt <willpratt78@gmail.com>
2020-03-24 14:17:03 -0700

    add operator's deinition and open/close tags

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
     $(".js-showing").toggle();
     $(".js-hidden").toggle();
   });
+
+  $(".clickable2").click(function() {
+    $(".oper-showing").toggle();
+    $(".oper-hidden").toggle();
+  });
+
+
 });
